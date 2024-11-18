a48b481f7714365845fc77fb67e04508d32bb834
Shanal Kevin Kumar <130121571+Sonkevin8@users.noreply.github.com>
2024-11-19 06:03:33 +1300

    3rd commit

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,16 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function App() {
-useEffect(() => {
-  fetch ('http://localhost:8081/users')
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-}, [])
+  // Use state to store the user data
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8081/users')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);  // Set the fetched data to the state
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div style={{ padding: '50px' }}>
+      <table>
+        <thead>
+          <tr>
+            <th>user_id</th>
+            <th>Username</th>
+            <th>Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user, index) => (
+            <tr key={index}>
+              <td>{user.user_id}</td>
+              <td>{user.username}</td>
+              <td>{user.password}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
+
 export default App;
