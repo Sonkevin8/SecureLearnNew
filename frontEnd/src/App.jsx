b4831c8a58cdfcf1ee5fce65a3736dbfd4e932fb
Shanal Kevin Kumar <130121571+Sonkevin8@users.noreply.github.com>
2024-11-19 05:10:53 +1300

    update dependencies and modify API fetch in App component

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,10 +2,15 @@ import React, { useEffect } from 'react'
 
 function App() {
 useEffect(() => {
-  fetch cv
+  fetch ('http://localhost:8081/users')
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
+}, [])
   return (
     <div>
       <h1>Hello World</h1>
     </div>
   )
 }
+export default App;
