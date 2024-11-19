ecc40b442a5d1ca53e5cd442ec1f40c8adc9edb9
Shanal Kevin Kumar <130121571+Sonkevin8@users.noreply.github.com>
2024-11-20 07:36:58 +1300

    Add login functionality and update dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const mysql=require('mysql');
 const cors=require('cors');
+const bodyParser = require('body-parser');
 
 const app=express();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const db=mysql.createConnection({
     host : 'localhost',
     user :"root",  
     password : "",
     database : "securelearn_db"
-})
+});
+
+db.connect((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('MySQL connected');
+  });
+
+  app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+  // Vulnerable SQL query
+  const sql = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    if (result.length > 0) {
+      res.send('Login successful');
+    } else {
+      res.send('Invalid credentials');
+    }
+  });
+});
+
+
+
+
+
 
 app.get('/',(req,res)=>{
     return res.json("From baCKEND sIDE");
