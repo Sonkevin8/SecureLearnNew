5128df2c452afcc8dfa4bab15ba7b0acea0c2e4d
kellythenoobycoder <135563310+kellythenoobycoder@users.noreply.github.com>
2024-11-22 20:05:52 +1300

    modified directory

--- frontEnd/src/SecureLearnNew/App.jsx
+++ frontEnd/src/SecureLearnNew/App.jsx
@@ -1,27 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import React from 'react';
-import { BrowswerRouter as Router, Routes, Route} from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Home from './pages/Home';
-import CyberSecurity from './pages/CyberSecurity';
-import Quizzes from './pages/Quizzes';
-import './App.css'; 
-
-const App = () => {
-    return (
-        <Router>
-            <div>
-                <Navbar />
-                <Routes>
-                    <Route path ="/" element = {<Home/>} />
-                    <Route path ="/material" element = {<CyberSecurity/>}/>
-                    <Route path ="/quizzes" element = {<Quizzes />}/>
-                </Routes>
-            </div>
-        </Router>
-    );
-
-}; 
+import React, { useEffect, useState } from 'react';
 
 function App() {
   // Use state to store the user data
