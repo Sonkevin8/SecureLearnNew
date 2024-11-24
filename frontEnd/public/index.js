8e0b84ae8c53dcf424c7103968b9a66a4ed731a5
kellythenoobycoder <135563310+kellythenoobycoder@users.noreply.github.com>
2024-11-24 19:28:17 +1300

    modified

--- frontEnd/public/index.js
+++ frontEnd/public/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import CyberSecurity from './pages/CyberSecurity';
+import Quizzes from './pages/Quizzes';
+import './App.css';
 
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/material" element={<CyberSecurity />} />
+          <Route path="/quizzes" element={<Quizzes />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 ReactDOM.render(
   <React.StrictMode>
     <App />
