925f4c9a3cfef43956efc5de5f34f1cc7047a387
kellythenoobycoder <135563310+kellythenoobycoder@users.noreply.github.com>
2024-11-24 23:10:17 +1300

    add cybersecurity

--- frontEnd/src/Pages/CyberSecurity.jsx
+++ frontEnd/src/Pages/CyberSecurity.jsx
@@ -5,11 +5,24 @@ const CyberSecurity = () => {
         <div>
             <h1>CyberSecurity Material</h1>
             <ul>
-                <li>What is cyber attack</li>
-                <li>What is cybersecurity</li>
-
+                <h3><li>What is cybersecurity?</li></h3>
+                <p>"Cybersecurity is the practice of protecting systems, networks, and programs from digital attacks. These attacks can include unauthorized access, data breaches, ransomware, and disruptions to normal business processes. The goal of cybersecurity is to ensure the confidentiality, integrity, and availability of data and systems." -Microsoft
+                    it can perform using the most classical popular cyber attact "phishing", - an email is sent to the target audience with a malicious link that asks you enter your personal / financial information. </p>
+                
+                <h3><li>What is a white hat hacker? </li></h3>
+                <p>A white hat hacke is also known as an ethical hacker, who could be working for an organisation or the government, who performs the attack to  the system/ networks to find out the vulnerability of the system/ networks and patch the vulnerabilites before the hackers would get into the system. The puprose of the white hacker is to improve the security of the system.   </p>
+                
+                <h3><li>How to become a white hat hacker</li></h3> 
+                <ul>
+                    <li>Have a strong Understanding of the fundamental computer systems, networking and porgramming </li>
+                    <li>Develop cybersecurity skills: Having a good knowledge of technical and soft skills</li>
+                    <li>Gain pratice experience: Perform some practical practices ie, IBM </li>
+                    <li>Stay current with the industry: ie keeping up to date with the latest development. </li>
+            
+                </ul>
             </ul>
         </div>
+
     );
 };
 
