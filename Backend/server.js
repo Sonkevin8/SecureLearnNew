0d76ac2a9e9041e189ee6477d35aecb04041cfcf
Malsyyy <malachaifenwick11@gmail.com>
2025-01-21 23:03:22 +1300

    Content page change
    Slight change to button

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: "root",
     password: "",
-    database: "securelearn_db"
+    database: "usernames"
 });
 
 db.connect((err) => {
